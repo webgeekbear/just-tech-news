cc995902cbe8926328452cd5d616939c95fe08e2
David Barron <90076120+webgeekbear@users.noreply.github.com>
2021-12-05 20:29:24 -0800

    Added encryption of password data

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@ const {
     Model,
     DataTypes
 } = require('sequelize');
+const bcrypt = require("bcrypt");
 const sequelize = require('../config/connection');
 
 // create our User model
@@ -46,6 +47,18 @@ User.init({
         }
     }
 }, {
+    hooks: {
+        // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+        // set up beforeUpdate lifecycle "hook" functionality
+        async beforeUpdate(updatedUserData) {
+            updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+            return updatedUserData;
+        }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
