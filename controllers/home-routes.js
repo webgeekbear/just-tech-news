329dd10f479c68e20a711cc2a37042141155cf18
David Barron <90076120+webgeekbear@users.noreply.github.com>
2021-12-16 10:44:59 -0800

    Add ability to log in/out

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -7,6 +7,8 @@ const {
 } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
+
     Post.findAll({
             attributes: [
                 'id',
@@ -42,6 +44,15 @@ router.get('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,8 +4,24 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 
+require('dotenv').config();
+
 const hbs = exphbs.create({});
 const app = express();
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
