3d97c94c1c9491ec5597d223218f8133df5189ff
David Barron <90076120+webgeekbear@users.noreply.github.com>
2021-12-15 16:40:32 -0800

    Added handlebars.js and templates

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
+const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -9,6 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
